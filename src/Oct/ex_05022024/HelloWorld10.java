3071f7c8358aeb595fde8ecdf29ab026500c4eac
hrushikeshharke <hrushikesh.harke12@gmail.com>
2024-10-06 12:13:58 +0530

    commit

---
+++ src/Oct/ex_05022024/HelloWorld10.java
@@ -0,0 +1,19 @@
+package Oct.ex_05022024;
+
+public class HelloWorld10
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World 01 !!!");
+        System.out.println("Hello World 02 !!!");
+        System.out.println("Hello World 03 !!!");
+        System.out.println("Hello World 03 !!!");
+        System.out.println("Hello World 04 !!!");
+        System.out.println("Hello World 05 !!!");
+        System.out.println("Hello World 06 !!!");
+        System.out.println("Hello World 07 !!!");
+        System.out.println("Hello World 08 !!!");
+        System.out.println("Hello World 09 !!!");
+        System.out.println("Hello World 10 !!!");
+    }
+}
---
+++ src/Oct/ex_05022024/MathOperations.java
@@ -0,0 +1,11 @@
+package Oct.ex_05022024;
+
+public class MathOperations {
+
+    public static void main(String[] args) {
+        int i = 6, j = 3;
+
+        System.out.println("Addition of 2 numbers" + (i+j));
+
+    }
+}
